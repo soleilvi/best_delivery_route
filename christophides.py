ffe4e703d3a8d5edb3ad3e4b6d0eac63c8540fd7
soleilvi <soleil.vivero@gmail.com>
2025-07-12 09:36:12 -0700

    cleaning up print() and comments in simplify_edges and is_disjoint

--- christophides.py
+++ christophides.py
@@ -109,9 +109,6 @@ def is_disjoint(node_connection_dict, current_node, complete_node_count):
         visited_nodes.add(current_node)
         current_node = unvisited_nodes.pop()
 
-    #DELETE
-    print(f"nodes in graph: {visited_nodes}, current graph count: {len(visited_nodes)}, OG graph count: 27")
-
     return len(visited_nodes) != complete_node_count
 
 
@@ -129,18 +126,14 @@ def reconnect_nodes(node_connection_dict, n, x, y):
 
 # Make a hamiltonian tour out of the Eulerian tour
 #TODO see if you can make it work without having to copy the list constantly 
-#TODO return graph (dictionary)
 def simplify_edges(distance_graph, eulerian_graph):
     node_connections = eulerian_graph.copy()
     nodes_over_two_edges = set()
 
     # 1) Find which nodes have more than two paths connected to them
-    # REMOVE EDGES HERE???? Wouldn't work since the indexes would get all messed up 
     for node in node_connections:
         if len(node_connections[node]) > 2:
             nodes_over_two_edges.add(node)
-
-    print(f"nodes over two edges: {nodes_over_two_edges}")
 
     # 2) Get the distances between all the destination nodes and put them in a priority queue 
     while nodes_over_two_edges:
@@ -150,36 +143,21 @@ def simplify_edges(distance_graph, eulerian_graph):
         n = len(destination_nodes)
         i = n - 1
 
-        print(f"current node: {node}")
-
-        # printing dict, delete once done
-        # for key in node_connections:
-        #     print(f"key: {key}, end points: {node_connections[key]}")
-        # print(nodes_over_two_edges)
-
         # Getting the distance between each destination node 
         for start_node in destination_nodes:
             # Prevent revisiting the same path several times and make runtime slightly faster with [(n-i):]
             for end_node in destination_nodes[(n-i):]:
-                # print(f"start node: {start_node}, end node: {end_node}")
                 destination_node_distances.append((distance_graph[start_node][end_node], start_node, end_node))  # Distance between start and end nodes
             i -= 1
 
         heapq.heapify(destination_node_distances)
 
-        # delete
-        # print(f"destination distances: {destination_node_distances}")
-
         # 3) Remove extra paths from the origin node by reconnecting those paths to the destinations with the smallest distance between them
         start_node = destination_node_distances[0][1]
         end_node = destination_node_distances[0][2]
-        print(f"(out) node: {node}, start_node: {start_node}, end_node: {end_node}")
         original_paths = {"node": node_connections[node].copy(), "start": node_connections[start_node].copy(), "end": node_connections[end_node].copy()}
 
         reconnect_nodes(node_connections, node, start_node, end_node)
-
-        print(node_connections[start_node])
-        print(node_connections[end_node])
 
         while is_disjoint(node_connections, start_node, len(distance_graph)):
             node_connections[node] = original_paths["node"].copy()
@@ -188,33 +166,22 @@ def simplify_edges(distance_graph, eulerian_graph):
 
             heapq.heappop(destination_node_distances)
 
-            #delete
-            print(f"path: {destination_node_distances[0]}")
-
             start_node = destination_node_distances[0][1]
             end_node = destination_node_distances[0][2]
             original_paths["start"] = node_connections[start_node].copy()
             original_paths["end"] = node_connections[end_node].copy()
 
-            # delete
-            print(f"node: {node}, start_node: {start_node}, end_node: {end_node}")
-            print(f"CONNECTIONS BEFORE: node: {node_connections[node]}, start_node: {node_connections[start_node]}, end_node: {node_connections[end_node]}")
-
             reconnect_nodes(node_connections, node, start_node, end_node)
 
-            # delete
-            print(f"CONNECTIONS AFTER: node: {node_connections[node]}, start_node: {node_connections[start_node]}, end_node: {node_connections[end_node]}")
-                
         # 5) Repeat until the current start node only has two distances (add the node to the set again if the length is still over 2)
         if len(node_connections[node]) > 2:
             nodes_over_two_edges.add(node)
 
-    print(is_disjoint(node_connections, 10, len(distance_graph)))
     return node_connections
 
 # TODO: this
 # Copy and paste the code in is_disjoint and just return the total distance idfk, I actually don't know what the purpose of this one is.
-def traverse_graph(graph):
+def traverse_graph():
     pass
 
 
